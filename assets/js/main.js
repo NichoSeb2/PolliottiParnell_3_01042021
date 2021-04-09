a43b7104b152c6bf6cce1410f0346ec1879d7391
NichoSeb2 <NichoSeb2@outlook.fr>
2021-04-09 18:00:26 +0200

    changed color palette, added author and description meta

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,6 @@
-$("button.navbar-toggler").on("click", (e) => {
+// has to be a regular function to have the correct "this" context
+$("button.navbar-toggler").on("click", function(e) {
 	$("#navbarNavAltMarkup").toggleClass("show");
+
+	$(this).blur();
 });
