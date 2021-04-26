072d34e63fabb990d065e9f9853ff2fdb590772a
NichoSeb2 <NichoSeb2@outlook.fr>
2021-04-26 10:19:21 +0200

    passed prettier on all file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 //! has to be a regular function to have the correct "this" context
-$("button.navbar-toggler").on("click", function(e) {
-	console.log("toggler clicked")
+$("button.navbar-toggler").on("click", function (e) {
+	console.log("toggler clicked");
 
 	$("#navBar").toggleClass("d-none");
 
